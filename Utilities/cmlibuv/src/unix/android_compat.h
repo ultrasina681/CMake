f16e4fa1014f3d523f49d3453e9f746407d74a2e
UltraSina <sketchwarefarsi@gmail.com>
2025-10-27 19:47:58 +0330

    f5

--- Utilities/cmlibuv/src/unix/android_compat.h
+++ Utilities/cmlibuv/src/unix/android_compat.h
@@ -3,19 +3,6 @@
 #if defined(__ANDROID__)
 #include <unistd.h>
 #include <fcntl.h>
-static inline int pipe2(int pipefd[2], int flags) {
-  int ret = pipe(pipefd);
-  if (ret == 0) {
-    if (flags & O_CLOEXEC) {
-      fcntl(pipefd[0], F_SETFD, FD_CLOEXEC);
-      fcntl(pipefd[1], F_SETFD, FD_CLOEXEC);
-    }
-    if (flags & O_NONBLOCK) {
-      fcntl(pipefd[0], F_SETFL, O_NONBLOCK);
-      fcntl(pipefd[1], F_SETFL, O_NONBLOCK);
-    }
-  }
-  return ret;
-}
+
 #endif
 #endif
